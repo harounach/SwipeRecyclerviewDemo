b2010799e8e6a908cd532d5cf044226f247ca2e3
Haroun <harounach8@gmail.com>
2017-05-06 00:51:23 +0200

    Initial commit

---
+++ app/src/main/java/com/ach/haroun/fujitsu/swiperecyclerviewdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.ach.haroun.fujitsu.swiperecyclerviewdemo;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+public class MainActivity extends AppCompatActivity {
+    MyRecyclerViewAdapter adapter;
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_id);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        adapter = new MyRecyclerViewAdapter(30);
+        recyclerView.setAdapter(adapter);
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+
+            }
+        }).attachToRecyclerView(recyclerView);
+
+    }
+}
